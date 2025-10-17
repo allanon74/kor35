6f10bce6b39d979ef6e258e1ab9b5f5e3eb240ce
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2025-10-17 14:56:39 +0200

    admin QR

--- oggetti/admin.py
+++ oggetti/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import QrCode
 
 # Register your models here.
+@admin.register
+class QrCodeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'data_creazione',)
+    readonly_fields = ('id', 'data_creazione',) 
+    
