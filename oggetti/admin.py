d8ccaea79cf26aa201efdfb8deda89bacb2fd9ac
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2025-10-17 15:01:28 +0200

    bugfix

--- oggetti/admin.py
+++ oggetti/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import QrCode
 
 # Register your models here.
-@admin.register
+@admin.register(QrCode)
 class QrCodeAdmin(admin.ModelAdmin):
     list_display = ('id', 'data_creazione',)
     readonly_fields = ('id', 'data_creazione',) 
