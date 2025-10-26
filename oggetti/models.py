ac221d25e1756b5e9a834f8c4cb715cf977f2080
Giorgio <giorgio.bertoluzza@gmail.com>
2025-10-26 15:00:49 +0100

    new QR

--- oggetti/models.py
+++ oggetti/models.py
@@ -1,11 +1,50 @@
 import uuid
-from django.db import models
+import secrets
+import string
+from django.db import models, IntegrityError
+
+def generate_short_id(length=14):
+    """
+    Genera un ID casuale sicuro di 14 caratteri.
+    Usa A-Z, a-z, 0-9.
+    """
+    alphabet = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(alphabet) for _ in range(length))
 
 # Create your models here.
 class QrCode(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(
+        primary_key=True,  # <-- QUESTO È IL PUNTO CHIAVE
+        max_length=14,
+        default=generate_short_id,
+        editable=False
+    )
     data_creazione = models.DateTimeField(auto_now_add=True)
     testo = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return "({codice})".format(codice=self.id)
+    
+    def save(self, *args, **kwargs):
+        """
+        Sovrascrive il metodo save per gestire le (improbabili)
+        collisioni della chiave primaria.
+        """
+        # _state.adding è True solo quando l'oggetto viene creato
+        if self._state.adding:
+            while True:  # Inizia un ciclo di tentativi
+                try:
+                    # Prova a salvare l'oggetto. 
+                    # L'ID è già stato generato dal 'default'.
+                    super().save(*args, **kwargs)
+                    # Se il salvataggio va a buon fine, usciamo dal ciclo
+                    break
+                except IntegrityError:
+                    # Se fallisce per IntegrityError, significa che l'ID esiste.
+                    # Generiamo un nuovo ID e il ciclo 'while' riproverà.
+                    self.id = generate_short_id()
+        else:
+            # Se non è un nuovo oggetto (_state.adding è False),
+            # è un aggiornamento. Salviamo normalmente.
+            super().save(*args, **kwargs)
