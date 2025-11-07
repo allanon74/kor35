95a51b2d5cc351cc02981ee71dfe759671833a9c
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2025-11-07 18:36:16 +0100

    correzione framework

--- kor35/settings.py
+++ kor35/settings.py
@@ -178,10 +178,11 @@ DATABASES = {
 	}
 }
 
+
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
 
 AUTHENTICATION_BACKENDS = [
