245507deb80c66f68ce388c137a0f5cdee36a916
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2025-11-13 02:28:03 +0100

    correzioni a static

--- kor35/settings.py
+++ kor35/settings.py
@@ -226,11 +226,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/django/kor35/static'
+STATIC_ROOT = '/var/www/django/static'
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static", 
 ]
 
 # Default primary key field type
