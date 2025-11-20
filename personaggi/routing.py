bed1d8970551b4c1162b503d96502b00b4c83bfb
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2025-11-20 16:26:18 +0100

    bug in import routing.py

--- personaggi/routing.py
+++ personaggi/routing.py
@@ -1,6 +1,6 @@
 # personaggi/routing.py
 from django.urls import re_path
-from ..kor35 import consumers
+from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
