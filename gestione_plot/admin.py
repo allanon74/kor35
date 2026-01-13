01a61497ccbbbf404315222e8344e2e0cc1e71a4
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2026-01-13 23:24:24 +0100

    correzione admin.py

--- gestione_plot/admin.py
+++ gestione_plot/admin.py
@@ -1,69 +1,107 @@
 from django.contrib import admin
 from .models import (
-    Evento, GiornoEvento, Quest, 
-    MostroTemplate, AttaccoTemplate, 
-    QuestMostro, PngAssegnato, QuestVista
+    Evento, GiornoEvento, Quest, QuestMostro, QuestVista,
+    MostroTemplate, AttaccoTemplate, PngAssegnato, 
+    StaffOffGame, QuestFase, QuestTask,
+    PaginaRegolamento
 )
 
-# --- INLINES PER TEMPLATE ---
+# --- INLINES (Per gestire sottocategorie dentro le pagine principali) ---
 
 class AttaccoTemplateInline(admin.TabularInline):
     model = AttaccoTemplate
     extra = 1
-    fields = ('ordine', 'nome_attacco', 'descrizione_danno')
-
-@admin.register(MostroTemplate)
-class MostroTemplateAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'punti_vita_base', 'armatura_base', 'guscio_base')
-    search_fields = ('nome',)
-    inlines = [AttaccoTemplateInline]
-
-# --- INLINES PER EVENTI E QUEST ---
 
 class GiornoEventoInline(admin.StackedInline):
     model = GiornoEvento
+    extra = 0
+    show_change_link = True
+
+class QuestTaskInline(admin.TabularInline):
+    model = QuestTask
     extra = 1
-    show_change_link = True # Permette di andare alla modifica del giorno
+    fields = ('nome', 'descrizione', 'is_opzionale', 'xp_reward')
+
+class QuestFaseInline(admin.StackedInline):
+    model = QuestFase
+    extra = 0
+    show_change_link = True
 
 class QuestMostroInline(admin.TabularInline):
     model = QuestMostro
     extra = 1
-    # Mostriamo i campi statistici così il Master può ritoccarli dopo la copia dal template
-    fields = ('template', 'staffer', 'punti_vita', 'armatura', 'guscio', 'note_per_staffer')
-    autocomplete_fields = ['template', 'staffer']
+    autocomplete_fields = ['mostro_template']
 
-class PngAssegnatoInline(admin.TabularInline):
-    model = PngAssegnato
-    extra = 1
-    autocomplete_fields = ['personaggio', 'staffer']
+# --- ADMIN CLASSES ---
 
-class QuestVistaInline(admin.TabularInline):
-    model = QuestVista
-    extra = 1
-    fields = ('tipo', 'manifesto', 'inventario', 'qr_code')
-    autocomplete_fields = ['manifesto', 'inventario', 'qr_code']
+@admin.register(PaginaRegolamento)
+class PaginaRegolamentoAdmin(admin.ModelAdmin):
+    list_display = ('titolo', 'slug', 'parent', 'ordine', 'public')
+    list_editable = ('ordine', 'public') # Permette modifica rapida dalla lista
+    list_filter = ('public', 'parent')
+    search_fields = ('titolo', 'contenuto')
+    prepopulated_fields = {'slug': ('titolo',)} # Compila lo slug mentre scrivi il titolo
+    ordering = ('parent', 'ordine', 'titolo')
+    fieldsets = (
+        ('Intestazione', {
+            'fields': ('titolo', 'slug', 'parent', 'ordine', 'public', 'immagine')
+        }),
+        ('Contenuto', {
+            'fields': ('contenuto',),
+            'description': 'Usa i placeholder {{WIDGET_...}} per inserire elementi dinamici.'
+        }),
+    )
 
-# --- ADMIN MODELS PRINCIPALI ---
+@admin.register(MostroTemplate)
+class MostroTemplateAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'punti_vita_base', 'armatura_base', 'guscio_base')
+    search_fields = ('nome', 'note_generali')
+    inlines = [AttaccoTemplateInline]
 
 @admin.register(Evento)
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titolo', 'data_inizio', 'data_fine', 'luogo')
-    filter_horizontal = ('partecipanti', 'staff_assegnato')
-    search_fields = ('titolo', 'luogo')
+    list_filter = ('data_inizio',)
+    search_fields = ('titolo',)
     inlines = [GiornoEventoInline]
+    filter_horizontal = ('staff_assegnato', 'partecipanti') # Widget migliore per i ManyToMany
 
 @admin.register(GiornoEvento)
 class GiornoEventoAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'data_ora_inizio', 'data_ora_fine')
+    list_display = ('evento', 'data_giorno', 'sinossi_breve')
     list_filter = ('evento',)
 
 @admin.register(Quest)
 class QuestAdmin(admin.ModelAdmin):
-    list_display = ('orario_indicativo', 'titolo', 'giorno')
-    list_filter = ('giorno__evento', 'giorno')
-    search_fields = ('titolo', 'descrizione_ampia')
-    # Qui il Master gestisce tutto il "Plot" della singola scena
-    inlines = [QuestMostroInline, PngAssegnatoInline, QuestVistaInline]
+    list_display = ('titolo', 'giorno', 'stato', 'staff_responsabile')
+    list_filter = ('stato', 'giorno__evento')
+    search_fields = ('titolo', 'descrizione')
+    inlines = [QuestFaseInline, QuestMostroInline]
+    autocomplete_fields = ['staff_responsabile']
 
-    def get_queryset(self, request):
-        return super().get_queryset(request).select_related('giorno__evento')
+@admin.register(QuestFase)
+class QuestFaseAdmin(admin.ModelAdmin):
+    list_display = ('titolo', 'quest', 'ordine')
+    list_filter = ('quest',)
+    inlines = [QuestTaskInline]
+
+@admin.register(PngAssegnato)
+class PngAssegnatoAdmin(admin.ModelAdmin):
+    list_display = ('nome_png', 'giocatore', 'quest', 'ruolo')
+    search_fields = ('nome_png', 'giocatore__username')
+    list_filter = ('ruolo',)
+
+@admin.register(StaffOffGame)
+class StaffOffGameAdmin(admin.ModelAdmin):
+    list_display = ('staffer', 'ruolo', 'quest')
+    list_filter = ('ruolo',)
+
+@admin.register(QuestVista)
+class QuestVistaAdmin(admin.ModelAdmin):
+    list_display = ('nome_vista', 'data_creazione')
+
+# QuestTask è gestito inline dentro QuestFase, ma se vuoi vederli tutti:
+@admin.register(QuestTask)
+class QuestTaskAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'fase', 'is_opzionale', 'is_completato')
+    list_filter = ('is_completato', 'fase__quest')
