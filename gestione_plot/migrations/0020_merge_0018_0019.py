a76e15a52997a5d5afdffea3ee357fda65e71c3f
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2026-03-19 17:59:22 +0100

    fix migrazioni

---
+++ gestione_plot/migrations/0020_merge_0018_0019.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("gestione_plot", "0018_wikimattoniwidget"),
+        ("gestione_plot", "0019_wikitierwidget_abilities_solo_list"),
+    ]
+
+    operations = []
+
