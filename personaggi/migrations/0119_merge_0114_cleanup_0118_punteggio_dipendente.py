bd34d6b2bea9184ca6385a498ec066767e0636a7
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2026-03-27 15:59:04 +0100

    fix errori

---
+++ personaggi/migrations/0119_merge_0114_cleanup_0118_punteggio_dipendente.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("personaggi", "0114_cleanup_ain_trait_slots"),
+        ("personaggi", "0118_abilita_punteggio_dipendente"),
+    ]
+
+    operations = []
+
