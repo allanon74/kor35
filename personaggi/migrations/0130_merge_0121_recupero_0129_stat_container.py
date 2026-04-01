0d547780177d0362167031183aaed70ee211c45d
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2026-04-01 15:08:18 +0200

    fix migrazioni

---
+++ personaggi/migrations/0130_merge_0121_recupero_0129_stat_container.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("personaggi", "0121_recupero_risorsa_auto"),
+        ("personaggi", "0129_statisticacontaineritem_is_dipendente"),
+    ]
+
+    operations = []
+
