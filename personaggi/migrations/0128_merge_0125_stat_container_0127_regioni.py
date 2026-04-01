98228aa9b117dafdd7f9dbe10549bd9cfdc05eb7
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2026-04-01 12:04:02 +0200

    correzione migrazioni

---
+++ personaggi/migrations/0128_merge_0125_stat_container_0127_regioni.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("personaggi", "0125_statisticacontaineritem_visibility_flags"),
+        ("personaggi", "0127_regioni_e_abilita_regione"),
+    ]
+
+    operations = []
+
