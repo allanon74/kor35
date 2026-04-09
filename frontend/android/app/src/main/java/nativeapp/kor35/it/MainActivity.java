8a3a9d0894f5196714a712b4ebcc966a9e1836ae
Giorgio Bertoluzza <giorgio.bertoluzza@gmail.com>
2026-04-09 08:35:35 +0200

    Revert "Integro migrazione monorepo in main."
    This reverts commit 85a805cff750352eb4b287276869d166e0bce6af, reversing
    changes made to 2cba340dc95c478879b88a41405663cc67e27ff5.

--- frontend/android/app/src/main/java/nativeapp/kor35/it/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package nativeapp.kor35.it;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  plugins: {
-    // Aggiornato per usare la sintassi corretta per le nuove versioni
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
-
